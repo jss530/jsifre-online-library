90106ce2c2c1102d3544bc24118999479bead41d
EC2 Default User <ec2-user@ip-172-31-39-222.us-east-2.compute.internal>
2018-01-10 23:48:56 +0000

    Added genre new form and custom attribute for books

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,4 +2,14 @@ class Genre < ApplicationRecord
   has_many :books
   
   validates :name, uniqueness: true
+  accepts_nested_attributes_for :books
+
+  def books_attributes=(books_attributes)
+    books_attributes.values.each do |book_attributes|
+      unless book_attributes[:title, :author].blank?
+      book = Book.find_or_create_by(book_attributes)
+      self.books << book
+      end
+    end
+  end
 end
