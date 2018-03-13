4f149ec1c9c84d30a0468ed11187da99ce76113f
jss530 <jsifre01@gmail.com>
2018-03-12 20:08:48 -0400

    Moved comments form AJAX to book.js file

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,6 +1,13 @@
+function Comment(json) {
+  debugger
+  this.body = json.name
+}
+
 Comment.prototype.renderLI = function() {
    "<li>" + Comment + "</li>"
 }
+ // need to define a comment class, establish what a comment instance is (constructor)
+ // check AJAX syntax
 
 $(function() {
   $("form#new_comment").on("submit", function(e) {
@@ -14,15 +21,18 @@ $(function() {
       data: params,
       dataType: "json",
       method: "POST"
-    })
-    .success(function(json) {
+      })
+      .done(function(json) {
+      debugger
       var comment = new Comment(json)
 
       var commentLI = comment.renderLI()
 
       $(".comments").append(commentLI)
+     })
     })
     .error(function(response) {
+      debugger
       console.log("error!", response)
     })
   })
