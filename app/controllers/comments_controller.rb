95384bf655fd37db26cc3bbff6d3928b2f71aaf9
jss530 <jsifre01@gmail.com>
2018-05-17 20:09:33 -0400

    took out require pry

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class CommentsController < ApplicationController
     before_action :authenticate_user!
     before_action :set_book
--- app/controllers/library_controller.rb
+++ app/controllers/library_controller.rb
@@ -1,10 +1,8 @@
-require 'pry'
-
 class LibraryController < ApplicationController
 
   def show
       @my_library = Library.find_or_create_by(:user_id => current_user.id)
-      
+
     if user_signed_in? && current_user.id == @my_library.user_id
         @rented_books = @my_library.user.rented_books
         @owned_books = @my_library.user.owned_books
